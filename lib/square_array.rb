56c27664c960514b30d69b487f3dea9760b85264
Jzavier Timm <jztimm@gmail.com>
2020-07-08 14:45:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,14 @@ def square_array(array)
   count = 0
   new_num_arr = []
   while count < array.length do
+<<<<<<< HEAD
     num = array[count]
     square = num ** 2    
     new_num_arr.push(square)
     count += 1
+=======
+    new_num_arr.push(array[count ** 2])
+>>>>>>> bead97466f66c70796ad52f28e91b9f787b023b2
   end
   puts new_num_arr
   new_num_arr
